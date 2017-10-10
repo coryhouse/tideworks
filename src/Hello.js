65e73df81bc99aaab9adab887b5e4000d3cba205
Cory House <Cory.House@coxautoinc.com>
2017-10-10 06:06:15 -0700

    convert

--- src/Hello.js
+++ src/Hello.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Hello({greeting='Hello', firstName}) {
-    return (
-        <div>
-            {greeting} {firstName}
-        </div>
-    )
+class Hello extends React.Component {
+    render() {
+        const {greeting, firstName} = this.props;
+        return (
+            <div>
+                {greeting} {firstName}
+            </div>
+        )
+    }
 }
 
 Hello.propTypes = {
     greeting: PropTypes.string,
     firstName: PropTypes.string.isRequired
+};
+
+Hello.defaultProps = {
+    greeting: 'Hello'
 };
 
 export default Hello;
