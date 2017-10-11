98c3245cb51349272d2e07cc15f29908aedcf51e
Cory House <Cory.House@coxautoinc.com>
2017-10-11 09:28:40 -0700

    Remove hot reload

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@
 
 import React from "react";
 import { render } from "react-dom";
-import { AppContainer } from "react-hot-loader";
 import configureStore, { history } from "./store/configureStore";
 import Root from "./components/Root";
 const store = configureStore();
 
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+  <Root store={store} history={history} />,
   document.getElementById("root")
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,10 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
+import { createStore, compose, applyMiddleware } from "redux";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+import createHistory from "history/createBrowserHistory";
 // 'routerMiddleware': the new way of storing route changes with redux middleware since rrV4.
-import { routerMiddleware } from 'react-router-redux';
-import rootReducer from '../reducers';
+import { routerMiddleware } from "react-router-redux";
+import rootReducer from "../reducers";
 export const history = createHistory();
 
 function configureStoreProd(initialState) {
@@ -47,19 +47,11 @@ function configureStoreDev(initialState) {
     composeEnhancers(applyMiddleware(...middlewares))
   );
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
-      store.replaceReducer(nextReducer);
-    });
-  }
-
   return store;
 }
 
 const configureStore =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === "production"
     ? configureStoreProd
     : configureStoreDev;
 
