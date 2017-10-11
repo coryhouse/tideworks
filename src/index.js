e593944000bef26fbad19ebd742b674648f667a1
Cory House <Cory.House@coxautoinc.com>
2017-10-11 09:06:28 -0700

    Remove hot reloading config for now since broken

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 /* eslint-disable import/default */
 
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
+import React from "react";
+import { render } from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import configureStore, { history } from "./store/configureStore";
+import Root from "./components/Root";
 const store = configureStore();
 
 render(
   <AppContainer>
     <Root store={store} history={history} />
   </AppContainer>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NewRoot = require('./components/Root').default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
