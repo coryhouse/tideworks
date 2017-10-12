250d68737e76db139c957a65836f7cb80ce49434
Cory House <Cory.House@coxautoinc.com>
2017-10-12 06:48:58 -0700

    Call action creator properly in thunk response

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -13,13 +13,13 @@ export function saveUser(user) {
   return function(dispatch) {
     // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
     // in this case at this point we could call a service that would persist the fuel savings
-    return dispatch(saveUserSuccess);
+    return dispatch(saveUserSuccess(user));
   };
 }
 
 export function deleteUser(id) {
   return function(dispatch) {
     // Assume API call is made here before calling dispatch...
-    return dispatch(deleteUserSuccess);
+    return dispatch(deleteUserSuccess(id));
   };
 }
